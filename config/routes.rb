b9f4ad9c91bb94cb48635cd6f13199a5507e060f
jrkinouani <kinouani@gmail.com>
2018-12-03 14:28:31 +0400

    add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root :to => 'pages#home'
   get '/apropos'  => 'pages#apropos'
   get "/mentions" => 'pages#mentions'
