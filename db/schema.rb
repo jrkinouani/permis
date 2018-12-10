186b7fa07e40c1371c1fb6185d7fd63e5564b7bd
jrkinouani <kinouani@gmail.com>
2018-12-10 14:36:40 +0400

    delete model customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205153515) do
+ActiveRecord::Schema.define(version: 20181210103421) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
