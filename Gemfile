6d4a3763b2eb84012eadb5ef1a63130c97054bf4
jrkinouani <kinouani@gmail.com>
2019-01-03 23:43:30 +0100

    config stripe

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
 gem 'rails_admin', '~> 1.3'
 gem 'devise'
+gem 'stripe'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,28 @@
+class ChargesController < ApplicationController
+def new
+   @reservation = Reservation.find(params["reservation_id"])
+end
+
+def create
+  @reservation = Reservation.find(params["reservation_id"])
+     # Amount in cents
+     @amount = @reservation.price.to_i * 100
+
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'eur'
+    )
+     @reservation = Reservation.find(params["reservation_id"])
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
