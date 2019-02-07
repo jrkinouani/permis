ebf599f7456b6a04e2f1ca81ef31203eb93254b6
jrkinouani <kinouani@gmail.com>
2019-02-07 14:28:13 +0400

    add damien mail mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,18 +1,18 @@
 class AdminMailer < ActionMailer::Base
   def new_reservation(reservation)
     @reservation = reservation
-    mail(from: 'biblioruncontact@gmail.com', to: 'kinouani@gmail.com', subject: "Nouvelle réservation !")
+    mail(from: 'damien.narsapa@permisapoints974.re', to: ['kinouani@gmail.com','damien.narsapa@permisapoints974.re'], subject: "Nouvelle réservation !")
   end
 
   def paiement(reservation)
     @reservation = reservation
-    mail(from: 'biblioruncontact@gmail.com', to: 'kinouani@gmail.com', subject: "'Paiement validé !")
+    mail(from: 'damien.narsapa@permisapoints974.re', to: ['kinouani@gmail.com','damien.narsapa@permisapoints974.re'], subject: "'Paiement validé !")
 
   end
 
   def user(reservation)
     @reservation = reservation
-    mail(from: 'biblioruncontact@gmail.com', to: @reservation.email, subject: "Convocation au stage !")
+    mail(from: 'damien.narsapa@permisapoints974.re', to: @reservation.email, subject: "Convocation au stage !")
   end
 
   def contact(message)
